3fefab9402211fc40b676691934a0379dbfe6d61
thisisstreamliner <thisisstreamliner@gmail.com>
2021-06-03 14:01:19 +0300

    Add Introduction Screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/screens/home_screen.dart';
+
+import 'screens/intro_auth_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,11 +12,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(body: Container(),),
+      home: NavigationPage()
+    );
+  }
+}
+
+class NavigationPage extends StatefulWidget {
+
+  const NavigationPage({ Key? key }) : super(key: key);
+
+  @override
+  _NavigationPageState createState() => _NavigationPageState();
+}
+
+class _NavigationPageState extends State<NavigationPage> {
+  bool isSigned = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: isSigned 
+        ? HomeScreen()
+        : IntroAuthScreen(),
     );
   }
 }
---
+++ lib/screens/intro_auth_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:zoom_clone/utils/app_styles.dart';
+
+class IntroAuthScreen extends StatefulWidget {
+  const IntroAuthScreen({ Key? key }) : super(key: key);
+
+  @override
+  _IntroAuthScreenState createState() => _IntroAuthScreenState();
+}
+
+class _IntroAuthScreenState extends State<IntroAuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return IntroductionScreen(
+      pages: [
+        PageViewModel(
+          title: 'Welcome',
+          body: 'Welcome to the best video conference app',
+          image: Center(child: Image.asset('images/welcome.png', height: 175,),),
+          decoration: PageDecoration(
+            bodyTextStyle: myStyle(20, Colors.black),
+            titleTextStyle: myStyle(20, Colors.black)
+          )
+        ),
+        PageViewModel(
+          title: 'Join or start meetings',
+          body: 'Easy to use interface, join to start meetings in a fast time',
+          image: Center(child: Image.asset('images/conference.png', height: 175,),),
+          decoration: PageDecoration(
+            bodyTextStyle: myStyle(20, Colors.black),
+            titleTextStyle: myStyle(20, Colors.black)
+          )
+        ),
+        PageViewModel(
+          title: 'Security',
+          body: 'Your security is important for us, Our servers are secure and reliable',
+          image: Center(child: Image.asset('images/secure.jpg', height: 175,),),
+          decoration: PageDecoration(
+            bodyTextStyle: myStyle(20, Colors.black),
+            titleTextStyle: myStyle(20, Colors.black)
+          )
+        )
+      ],
+      onDone: (){
+
+      },
+      onSkip: (){
+
+      },
+      showSkipButton: true,
+      skip: const Icon(Icons.skip_next, size: 45,),
+      next: const Icon(Icons.arrow_forward_ios),
+      done: Text('Done', style: myStyle(20, Colors.black),),
+    );
+  }
+}
